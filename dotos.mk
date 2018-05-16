fe54511548a301ba39f0bf0e978606ea0f7fff45
Oleg <olegano.bigstone@gmail.com>
2018-05-16 15:37:09 +1000

    dot

---
+++ dotos.mk
@@ -0,0 +1,37 @@
+# Boot animation
+TARGET_SCREEN_HEIGHT := 1920
+TARGET_SCREEN_WIDTH := 1080
+
+# Inherit some common LineageOS stuff.
+$(call inherit-product, vendor/lineage/config/common.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/lge/bullhead/aosp_bullhead.mk)
+
+## Device identifier. This must come after all inclusions
+PRODUCT_NAME := dot_bullhead
+PRODUCT_BRAND := google
+PRODUCT_MODEL := Nexus 5X
+TARGET_MANUFACTURER := LGE
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME=bullhead \
+    BUILD_FINGERPRINT=google/bullhead/bullhead:8.1.0/OPM4.171019.016.A1/4720815:user/release-keys \
+    PRIVATE_BUILD_DESC="bullhead-user 8.1.0 OPM4.171019.016.A1 4720815 release-keys"
+
+# Google property overides
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.control_privapp_permissions=permissive \
+    ro.url.legal=http://www.google.com/intl/%s/mobile/android/basic/phone-legal.html \
+    ro.url.legal.android_privacy=http://www.google.com/intl/%s/mobile/android/basic/privacy.html \
+    ro.com.google.clientidbase=android-google \
+    ro.error.receiver.system.apps=com.google.android.gms \
+    ro.setupwizard.enterprise_mode=1 \
+    ro.com.android.dataroaming=false \
+    ro.atrace.core.services=com.google.android.gms,com.google.android.gms.ui,com.google.android.gms.persistent \
+    ro.setupwizard.rotation_locked=true \
+    ro.opa.eligible_device=true
+
+
+# Unofficial build ID
+	TARGET_UNOFFICIAL_BUILD_ID := BigStone
